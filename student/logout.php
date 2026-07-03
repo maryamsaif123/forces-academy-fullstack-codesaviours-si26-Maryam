5d4b885271a7064a226b0dee29855628dca4d497
Maryam Saif <maryamsaif123.com>
2026-07-03 19:37:03 +0500

    Add student logout functionality

--- student/logout.php
+++ student/logout.php
@@ -1,10 +1,18 @@
+
 <?php
 
 session_start();
 
+$_SESSION = array();
+
 session_destroy();
 
-header("location:login.php");
+header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: 0");
+
+header("Location: login.php");
+exit();
+
 
 ?>
-
